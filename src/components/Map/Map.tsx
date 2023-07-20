9bca08ec8a976a2981cf1d301a082eebc91d6478
ShuVeriDa <bashtarov@outlook.com>
2023-07-21 01:13:14 +0300

    Added centering when you select a point

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,9 @@
 import {IRoute} from "../../types/types.ts";
-import {FC} from "react";
+import {FC, useEffect, useRef} from "react";
 import {MapContainer, Marker, Polyline, TileLayer} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import styles from './Map.module.scss';
-import {LatLngTuple} from "leaflet";
+import L, {LatLngTuple} from "leaflet";
 
 interface IMapProps {
   currentRoute: IRoute;
@@ -11,11 +11,22 @@ interface IMapProps {
 }
 
 export const Map: FC<IMapProps> = ({ currentRoute, polyline }) => {
+  const mapRef = useRef<MapContainer>(null);
+
+  useEffect(() => {
+    if (mapRef.current && currentRoute.points.length > 0) {
+      const bounds = L.latLngBounds(currentRoute.points.map((point) => [point.lat, point.lng]));
+      mapRef.current.fitBounds(bounds, { padding: [50, 50] });
+    }
+  }, [currentRoute]);
+
   return (
     <div className={styles.wrapper}>
-      <MapContainer center={[currentRoute.points[2].lat, currentRoute.points[2].lng]}
-                    zoom={12}
-                    className={styles.map}
+      <MapContainer
+        ref={mapRef}
+        center={[currentRoute.points[0].lat, currentRoute.points[0].lng]}
+        zoom={12}
+        className={styles.map}
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         {currentRoute && (
